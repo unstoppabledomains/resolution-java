64aab4e3e4c299573c654a833a785b3a0cb8bca4
Gentelman John <jeyhunt@gmail.com>
2020-10-26 14:49:09 -0700

    remove abstract from namingService interface

--- src/main/java/com/unstoppabledomains/resolution/NamingService.java
+++ src/main/java/com/unstoppabledomains/resolution/NamingService.java
@@ -3,10 +3,10 @@ package com.unstoppabledomains.resolution;
 import com.unstoppabledomains.exceptions.NamingServiceException;
 
 abstract interface NamingService {
-    abstract Boolean isSupported(String domain);
-    abstract String addr(String domain, String ticker) throws NamingServiceException;
-    abstract String ipfsHash(String domain) throws NamingServiceException;
-    abstract String email(String domain) throws NamingServiceException;
-    abstract String owner(String domain) throws NamingServiceException;
-    abstract String namehash(String domain) throws NamingServiceException;
+    Boolean isSupported(String domain);
+    String addr(String domain, String ticker) throws NamingServiceException;
+    String ipfsHash(String domain) throws NamingServiceException;
+    String email(String domain) throws NamingServiceException;
+    String owner(String domain) throws NamingServiceException;
+    String namehash(String domain) throws NamingServiceException;
 }
