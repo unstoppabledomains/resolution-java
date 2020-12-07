c9b2d5b398779d0337de6df018b7027f9c5db6b0
Gentelman John <jeyhunt@gmail.com>
2020-12-07 14:15:33 -0800

    fixed jsdocs warning

--- src/main/java/com/unstoppabledomains/resolution/Namehash.java
+++ src/main/java/com/unstoppabledomains/resolution/Namehash.java
@@ -55,7 +55,7 @@ public class Namehash {
      * href="http://docs.ens.domains/en/latest/implementers.html#normalising-and-validating-names">specification</a>.
      *
      * @param domain our user input blockchain domain
-     * @throws NamingServiceException
+     * @throws NamingServiceException - when domain is not supported
      * @return normalised blockchain domain
      */
     public static String normalise(String domain) throws NamingServiceException {
