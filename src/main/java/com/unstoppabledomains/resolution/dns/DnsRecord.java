e6dc44759f3c734e4878158c258dd61460f89c60
Gentelman John <jeyhunt@gmail.com>
2020-12-14 10:07:47 -0800

    EqualsAndHashCode from lombok

--- src/main/java/com/unstoppabledomains/resolution/dns/DnsRecord.java
+++ src/main/java/com/unstoppabledomains/resolution/dns/DnsRecord.java
@@ -3,19 +3,14 @@ package com.unstoppabledomains.resolution.dns;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.EqualsAndHashCode;
 
 @Getter
 @AllArgsConstructor
 @ToString
+@EqualsAndHashCode
 public class DnsRecord {
   private DnsRecordsType type;
   private Number ttl;
   private String data;
-
-  @Override
-  public boolean equals(Object obj) {
-    DnsRecord that = (DnsRecord) obj;
-
-    return that.type.equals(type) && that.ttl.equals(ttl) && that.data.equals(data);
-  }
 }
