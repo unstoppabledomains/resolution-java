82e16f400daacd0dc83da05becf8c64f31cebbd2
DancingAxolotl <5020980+DancingAxolotl@users.noreply.github.com>
2022-07-21 18:02:45 +0200

    Pull uns configs on gradle build

--- src/test/java/com/unstoppabledomains/config/KnownRecordsTest.java
+++ src/test/java/com/unstoppabledomains/config/KnownRecordsTest.java
@@ -18,7 +18,7 @@ public class KnownRecordsTest {
     void shouldLoadCorrectRecords() {
         String versionFromCode = KnownRecords.getVersion();
         // we want to keep this hardcoded and manually updated to test if the Client.getVersion correctly reads the .json file;
-        String versionFromFile = "2.1.1";
+        String versionFromFile = "2.1.4";
         assertEquals(versionFromFile, versionFromCode);
     }
 }
