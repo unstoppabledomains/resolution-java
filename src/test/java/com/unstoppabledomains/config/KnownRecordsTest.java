0c359db03c23078686b300fc1eca24db9482d19f
DancingAxolotl <5020980+DancingAxolotl@users.noreply.github.com>
2023-02-03 17:47:09 +0100

    Upgrade configs & fix tests

--- src/test/java/com/unstoppabledomains/config/KnownRecordsTest.java
+++ src/test/java/com/unstoppabledomains/config/KnownRecordsTest.java
@@ -18,7 +18,7 @@ public class KnownRecordsTest {
     void shouldLoadCorrectRecords() {
         String versionFromCode = KnownRecords.getVersion();
         // we want to keep this hardcoded and manually updated to test if the Client.getVersion correctly reads the .json file;
-        String versionFromFile = "2.1.12";
+        String versionFromFile = "2.1.23";
         assertEquals(versionFromFile, versionFromCode);
     }
 }
--- src/test/java/com/unstoppabledomains/resolution/ResolutionBuilderTest.java
+++ src/test/java/com/unstoppabledomains/resolution/ResolutionBuilderTest.java
@@ -43,8 +43,8 @@ public class ResolutionBuilderTest {
 
     @Test
     public void buildsWithDefaultParams() throws Exception {
-        NSConfig expectedUNSL1Config = new NSConfig(Network.MAINNET, "https://mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x1BDc0fD4fbABeed3E611fd6195fCd5d41dcEF393");
-        NSConfig expectedUNSL2Config = new NSConfig(Network.MATIC_MAINNET, "https://polygon-mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x3E67b8c702a1292d1CEb025494C84367fcb12b45");
+        NSConfig expectedUNSL1Config = new NSConfig(Network.MAINNET, "https://mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x578853aa776Eef10CeE6c4dd2B5862bdcE767A8B");
+        NSConfig expectedUNSL2Config = new NSConfig(Network.MATIC_MAINNET, "https://polygon-mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x91EDd8708062bd4233f4Dd0FCE15A7cb4d500091");
         NSConfig expectedZNSConfig = new NSConfig(Network.MAINNET, "https://api.zilliqa.com", "0x9611c53BE6d1b32058b2747bdeCECed7e1216793");
 
         ResolutionBuilder builder = new ResolutionBuilder(mockConnector);
@@ -89,8 +89,8 @@ public class ResolutionBuilderTest {
 
     @Test
     public void allowsToCustomizeOneService() throws Exception {
-        NSConfig expectedUNSL1Config = new NSConfig(Network.MAINNET, "https://mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x1BDc0fD4fbABeed3E611fd6195fCd5d41dcEF393");
-        NSConfig expectedUNSL2Config = new NSConfig(Network.MATIC_MAINNET, "https://polygon-mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x3E67b8c702a1292d1CEb025494C84367fcb12b45");
+        NSConfig expectedUNSL1Config = new NSConfig(Network.MAINNET, "https://mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x578853aa776Eef10CeE6c4dd2B5862bdcE767A8B");
+        NSConfig expectedUNSL2Config = new NSConfig(Network.MATIC_MAINNET, "https://polygon-mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x91EDd8708062bd4233f4Dd0FCE15A7cb4d500091");
         NSConfig expectedZNSConfig = new NSConfig(Network.ZIL_TESTNET, "https://dev-api.zilliqa.com", "0x00000000000000000000000000000000000000004");
 
         ResolutionBuilder builder = new ResolutionBuilder(mockConnector);
@@ -132,8 +132,8 @@ public class ResolutionBuilderTest {
 
     @Test
     public void pullsContractAddressFromConfig() throws Exception {
-        NSConfig expectedUNSL1Config = new NSConfig(Network.MAINNET, "https://mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x1BDc0fD4fbABeed3E611fd6195fCd5d41dcEF393");
-        NSConfig expectedUNSL2Config = new NSConfig(Network.MATIC_MAINNET, "https://polygon-mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x3E67b8c702a1292d1CEb025494C84367fcb12b45");
+        NSConfig expectedUNSL1Config = new NSConfig(Network.MAINNET, "https://mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x578853aa776Eef10CeE6c4dd2B5862bdcE767A8B");
+        NSConfig expectedUNSL2Config = new NSConfig(Network.MATIC_MAINNET, "https://polygon-mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x91EDd8708062bd4233f4Dd0FCE15A7cb4d500091");
 
         ResolutionBuilder builder = new ResolutionBuilder(mockConnector);
         builder
