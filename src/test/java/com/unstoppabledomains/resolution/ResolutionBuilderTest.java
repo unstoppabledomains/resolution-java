5f84a14dc56d612c90b5a30ff860659cd71b3f98
tunguyen <tu@unstoppabledomains.com>
2023-03-14 10:33:38 -0400

    [DT-1119] Use github action env var for test (#89)
    * Use github action env var for test
    
    * Use env var rpc urls in other tests
    
    * Update versioning

--- src/test/java/com/unstoppabledomains/resolution/ResolutionBuilderTest.java
+++ src/test/java/com/unstoppabledomains/resolution/ResolutionBuilderTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 
 import java.util.Map;
 
+import com.unstoppabledomains.TestUtils;
 import com.unstoppabledomains.config.network.model.Network;
 import com.unstoppabledomains.resolution.naming.service.NSConfig;
 import com.unstoppabledomains.resolution.naming.service.NamingService;
@@ -61,8 +62,8 @@ public class ResolutionBuilderTest {
 
     @Test
     public void buildsWithCustomParams() throws Exception {
-        NSConfig expectedUNSL1Config = new NSConfig(Network.GOERLI, "https://goerli.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x00000000000000000000000000000000000000001");
-        NSConfig expectedUNSL2Config = new NSConfig(Network.MUMBAI_TESTNET, "https://polygon-mumbai.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x00000000000000000000000000000000000000002");
+        NSConfig expectedUNSL1Config = new NSConfig(Network.GOERLI, TestUtils.getL1TestProviderUrl(), "0x00000000000000000000000000000000000000001");
+        NSConfig expectedUNSL2Config = new NSConfig(Network.MUMBAI_TESTNET, TestUtils.getL2TestProviderUrl(), "0x00000000000000000000000000000000000000002");
         NSConfig expectedZNSConfig = new NSConfig(Network.ZIL_TESTNET, "https://dev-api.zilliqa.com", "0x00000000000000000000000000000000000000004");
 
         ResolutionBuilder builder = new ResolutionBuilder(mockConnector);
@@ -132,13 +133,13 @@ public class ResolutionBuilderTest {
 
     @Test
     public void pullsContractAddressFromConfig() throws Exception {
-        NSConfig expectedUNSL1Config = new NSConfig(Network.MAINNET, "https://mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x578853aa776Eef10CeE6c4dd2B5862bdcE767A8B");
-        NSConfig expectedUNSL2Config = new NSConfig(Network.MATIC_MAINNET, "https://polygon-mainnet.infura.io/v3/e0c0cb9d12c440a29379df066de587e6", "0x91EDd8708062bd4233f4Dd0FCE15A7cb4d500091");
+        NSConfig expectedUNSL1Config = new NSConfig(Network.GOERLI, TestUtils.getL1TestProviderUrl(), "0x76007c52C73972A441aFA1A0E1016B140ffdE689");
+        NSConfig expectedUNSL2Config = new NSConfig(Network.MUMBAI_TESTNET, TestUtils.getL2TestProviderUrl(), "0xBD4674F11d512120dFc8BAe5f84963d7419A5db2");
 
         ResolutionBuilder builder = new ResolutionBuilder(mockConnector);
         builder
-            .unsChainId(UNSLocation.Layer1, Network.MAINNET)
-            .unsChainId(UNSLocation.Layer2, Network.MATIC_MAINNET)
+            .unsChainId(UNSLocation.Layer1, Network.GOERLI)
+            .unsChainId(UNSLocation.Layer2, Network.MUMBAI_TESTNET)
             .unsProviderUrl(UNSLocation.Layer1, expectedUNSL1Config.getBlockchainProviderUrl())
             .unsProviderUrl(UNSLocation.Layer2, expectedUNSL2Config.getBlockchainProviderUrl())
             .build();
