fa666e8ac2ba461f8f3152367ee3b203363daf43
Tu Nguyen <tu@unstoppabledomains.com>
2023-05-08 14:35:50 -0400

    Try update records

--- src/test/java/com/unstoppabledomains/config/KnownRecordsTest.java
+++ src/test/java/com/unstoppabledomains/config/KnownRecordsTest.java
@@ -18,7 +18,7 @@ public class KnownRecordsTest {
     void shouldLoadCorrectRecords() {
         String versionFromCode = KnownRecords.getVersion();
         // we want to keep this hardcoded and manually updated to test if the Client.getVersion correctly reads the .json file;
-        String versionFromFile = "2.1.25";
+        String versionFromFile = "2.1.26";
         assertEquals(versionFromFile, versionFromCode);
     }
 }
